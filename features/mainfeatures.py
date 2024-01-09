e0371e36e7d54180553710f3cba674c6ae5bf45f
Dirushan <116972461+d1-create@users.noreply.github.com>
2024-01-09 20:31:17 +0000

    Add more comments and fix bugs

--- features/mainfeatures.py
+++ features/mainfeatures.py
@@ -2,18 +2,24 @@ import features.operators as calcfunctions
 
 #get input for what operation to do
 def operationinput():
-    operations = ["add","subtract","divide","multiply","other"] #list of operations for easy elif statements
-    operator = input("Operation:Add,Subtract,Divide,Multiply or Other? ").lower() #get input and lowercase it
-    
-    if(operator in operations):#if it is processable then do operation
+    #list of operations for easy elif statements
+    operations = ["add","subtract","divide","multiply","other"] 
+    #get input and lowercase it
+    operator = input("Operation:Add,Subtract,Divide,Multiply or Other? ").lower() 
+
+    #if it is processable then do operation
+    if(operator in operations):
         return operator
+    
+    #if it cannot be processed then quit the application
     elif(operator not in operations):
         print("Incorrect input, please restart program")
         quit()
 
 #process the data in accordance to the features
 def processinput(operator):
-    operations = ["add","subtract","divide","multiply","other"] #list of operations for easy elif statements
+    #list of operations for easy elif statements
+    operations = ["add","subtract","divide","multiply","other"] 
     
     #input for 2 numbers
     num1 = float(input("What is your first number "))
@@ -28,4 +34,9 @@ def processinput(operator):
         output = calcfunctions.Divide(num1,num2)
     elif(operator == operations[3]):#if multiply
         output = calcfunctions.Multiply(num1,num2)
+
+    #return the output and two numbers for printing
     return output,num1,num2
+
+def output(operator,answer,num1,num2):
+    print(f"You tried to {operator} {num1} and {num2} so you got {answer} as the answer")
--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 import features.mainfeatures as mainfeatures
 
 def main():
+    #find what operation the user wants to do
     operator = mainfeatures.operationinput()
-    answer,num1,num2 = mainfeatures.processinput(operator)
-    print(f"You tried to {operator} {num1} and {num2} so you got an answer of {answer}")
 
+    #process the input with numbers to do the calculation
+    answer,num1,num2 = mainfeatures.processinput(operator)
+    
+    #output the text showing the answer
+    mainfeatures.output(operator,answer,num1,num2)
 
 main()
